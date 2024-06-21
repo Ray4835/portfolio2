866f5b25cb8b833e72f0fd0caf58682b004d8c9e
Rayssa <115266145+Ray4835@users.noreply.github.com>
2024-06-21 19:07:10 -0300

    Update research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -7,7 +7,7 @@ const research = [
     title: "Licor de leite",
     authors: "Rayssa Machado, Marcos Paulo, Mariana Fragoso e Thais",
     conferences: "Preparação realizada na aula de Nutrição e Higiene dos Alimento orientada pela professora Carla Keiko",
-    researchYr: 07 - dez,2023,
+    researchYr: 07 - 10 - 2023,
     citebox: "popup1",
     image: "images/Licor.jpeg",
     citation: {
